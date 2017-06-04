19dd8e3f88ab2ad941b22850e7af8cee22d06d75
John Hunter <john@pushagency.io>
2017-06-04 12:30:09 -0400

    cleanup

--- src/main/java/TypicalProducer.java
+++ src/main/java/TypicalProducer.java
@@ -16,9 +16,13 @@ public class TypicalProducer extends AbstractProducer {
     @Override
     String generate() {
         StringBuilder builder = new StringBuilder();
-        builder.append(random.nextInt(256) + "." + random.nextInt(256) + "." + random.nextInt(256) + "." + random.nextInt(256));
+        builder.append(random.nextInt(256) + "." +
+                random.nextInt(256) + "." +
+                random.nextInt(256) + "." +
+                random.nextInt(256));
         builder.append(" - - [");
-        builder.append(LocalDateTime.now().minusSeconds(1).format(DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss")));
+        builder.append(LocalDateTime.now().minusSeconds(1)
+                .format(DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss")));
         builder.append("] GET / HTTP/1.0 200 1783");
         return builder.toString();
     }
